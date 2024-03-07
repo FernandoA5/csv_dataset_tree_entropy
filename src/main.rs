7ac45c424c1f1cc3756cb00f49f6b0153c4df56b
FernandoA5 <fernandoalcalacasas@hotmail.com>
2024-03-07 17:10:17 -0600

    Creado algoritmo de combinatoria de árbol (Estático)

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,15 @@ struct Vector{
     col: Vec<String>,
 
 }
+
+//DEFINIMOS EL PATH DEL CSV COMO UNA CONSTANTE:
+const PATH: &str = "src/titanic.csv";
+
 fn main() {
     //Leemos el CSV:
     let mut headers: Vec<String> = Vec::new();
     let mut vectores: Vec<Vector> = Vec::new();
-    read_csv_vector(&mut vectores, &mut headers);
+    read_csv_vector(&mut vectores, &mut headers, PATH);
     
     //Obtenemos la combinatoria de las columnas:
     let combinaciones: usize= (1..=headers.len()).product();
@@ -24,6 +28,57 @@ fn main() {
     for (i, valores_unicos) in valores_unicos_por_columna.iter().enumerate() {
         println!("Valores únicos de la columna {}: {:?}", headers[i], valores_unicos); //To-delete
     }
+
+    //Esto recorre de 0 a N columnas:
+    for num_col in 0..headers.len(){
+        println!("Columna: {}", headers[num_col]);
+        let mut columnas = headers.clone();
+        //Eliminamos la columna actual que coincida con el valor de headers[num_col]:
+        columnas = columnas.into_iter().filter(|x| *x != headers[num_col]).collect();
+
+        
+
+        for col_i in columnas.clone() {
+            println!("  Columna: {}", col_i); //To-delete
+            let columnas_i = columnas.clone();
+            //Eliminamos la columna actual que coincida con el valor de headers[num_col]:
+            let columnas_i: Vec<String> = columnas_i.into_iter().filter(|x| *x != col_i).collect();
+
+          
+
+            for col_j in columnas_i.clone() {
+                println!("    Columna: {}", col_j); //To-delete
+                let columnas_j = columnas_i.clone();
+                //Eliminamos la columna actual que coincida con el valor de headers[num_col]:
+                let columnas_j: Vec<String> = columnas_j.into_iter().filter(|x| *x != col_j).collect();
+
+                
+
+                for col_k in columnas_j.clone() {
+                    println!("      Columna: {}", col_k); //To-delete
+                    let columnas_k = columnas_j.clone();
+                    //Eliminamos la columna actual que coincida con el valor de headers[num_col]:
+                    let columnas_k: Vec<String> = columnas_k.into_iter().filter(|x| *x != col_k).collect();
+                    if columnas_k.len() == 0{
+                        break;
+                    }
+                }
+
+                if columnas_j.len() == 1{
+                    break;
+                }
+            }
+
+            if columnas_i.len() == 1{
+                break;
+            }
+        }
+
+        if columnas.len() == 1{
+            break;
+        }
+    }
+
 }
 
 
@@ -43,10 +98,10 @@ fn obtener_valores_unicos_por_columna(vectores: &Vec<Vector>, headers: &Vec<Stri
     }
 }
 
-fn read_csv_vector(vector: &mut Vec<Vector>, headers: &mut Vec<String>){
+fn read_csv_vector(vector: &mut Vec<Vector>, headers: &mut Vec<String>, path: &str){
     let mut rdr = csv::ReaderBuilder::new()
         .has_headers(true)
-        .from_path("src/titanic.csv")
+        .from_path(path)
         .unwrap();
 
     //Obtenemos los headers:
