dde8f39e2174edfa1655a6f9c40c7fcbc2b6c229
FernandoA5 <fernandoalcalacasas@hotmail.com>
2024-03-11 20:30:36 -0600

    Agregado funcionamiento de cálculo de frecuencias, y entropias a los primeros 3 niveles del árbol estático

--- src/main.rs
+++ src/main.rs
@@ -36,20 +36,210 @@ fn main() {
     println!("########################----VERSIÓN ESTÁTICA----########################");
     let mut contador = 0;
     //Esto recorre de 0 a N columnas:
-    for num_col in 0..headers.len(){
-        println!("───Columna: {}", headers[num_col]);
+    // for num_col in 0..headers.len()
+    for num_col in 0..1{
+        println!("───{}", headers[num_col]);
+
+        //CALCULOS DE COLUMNA ROOT:
+        let valores_unicos = valores_unicos_por_columna[num_col].clone();
+        println!("Valores únicos de la columna {}: {:?}", headers[num_col], valores_unicos); //To-delete
+        //Ahora buscamos todas las apariciones de cada valor único en la columna:
+        //PARA ESO RECORREMOS LOS VALORES ÚNICOS Y POR CADA UNO IMPRIMIMOS LO SIGUIENTE:
+        //"Apariciones de {nombre_columna} {valor}: {apariciones}"
+        //BAJO NINGÚN CONCEPTO LOS IMPRIMAS COMO TUPLAS:
+        let mut vec_apariciones: Vec<(String, usize)> = Vec::new();
+        for valor in valores_unicos.iter(){
+            let apariciones = vectores.iter().filter(|x| x.header == headers[num_col])
+            .flat_map(|x| x.col.iter()).filter(|x| *x == valor).count();
+            vec_apariciones.push((valor.to_string(), apariciones));
+            println!("Apariciones de {} {}: {}", headers[num_col], valor, apariciones);
+        }
+        
+
+
+        
+
+        //Calculamos la probabilidad de cada valor único, lo imprimimos de la misma forma que las apariciones:
+        let total_apariciones: usize = vec_apariciones.iter().map(|x| x.1).sum();
+        let mut probabilidad: Vec<f64> = Vec::new();
+        for aparicion in vec_apariciones.iter(){
+            probabilidad.push(aparicion.1 as f64 / total_apariciones as f64);
+            println!("Probabilidad de {} {}: {}", headers[num_col], aparicion.0, aparicion.1 as f64 / total_apariciones as f64);
+        }
+        
+
+        //Calculamos la entropía de cada valor único, lo imprimimos de la misma forma que las apariciones:
+        let mut entropia: Vec<f64> = Vec::new();
+        for (i, prob) in probabilidad.iter().enumerate(){
+            let entropia_i:f64;
+            //VALIDAMOS QUE LA ENTROPIA NO SEA 0
+            if *prob == 0.0 {
+                entropia_i = 0.0;
+                entropia.push(entropia_i);
+            }
+            else{
+                entropia_i = -prob * prob.log10();
+                entropia.push(entropia_i);
+            }
+            println!("Entropía de {} {}: {}", headers[num_col], vec_apariciones[i].0, entropia_i);
+        }
+
+
+        //Entropía de la columna:
+        let entropia_columna: f64 = entropia.iter().sum();
+        println!("Entropía de la columna {}: {}", headers[num_col], entropia_columna); //To-delete
+
+
+
+
         let mut columnas = headers.clone();
         //Eliminamos la columna actual que coincida con el valor de headers[num_col]:
         columnas = columnas.into_iter().filter(|x| *x != headers[num_col]).collect();
 
         for col_i in columnas.clone() {
-            println!("   └─── Columna: {}", col_i); //To-delete
+            //"   └─── {padre}-{hija}", col_i
+            println!("   └─── {}-{}", headers[num_col], col_i); //To-delete
+
+            //CALCULOS DE COLUMNA I:
+            let valores_unicos_i = valores_unicos_por_columna[headers.iter().position(|x| *x == col_i).unwrap()].clone();
+            println!("   Valores únicos de la columna {}: {:?}", col_i, valores_unicos_i); //To-delete
+            //COMBINACIONES DE LOS VALORES ÚNICOS DE LA COLUMNA I y la columna ROOT Vec::<String, String> = (root[0], col_i[0]), (root[0], col_i[1]), (root[1], col_i[0]), (root[1], col_i[1]):
+            let mut combinaciones_valores_unicos: Vec<(String, String)> = Vec::new();
+            let mut apariciones_combinaciones: Vec<usize> = Vec::new();
+            for valor_root in valores_unicos.iter(){
+                for valor_i in valores_unicos_i.iter(){
+                    combinaciones_valores_unicos.push((valor_root.to_string(), valor_i.to_string()));
+                    //BUSCAMOS LAS APARICIONES DE CADA COMBINACIÓN ROOT && I: Usize (Sin reiterar, con lo que ya tenemos)
+                    let apariciones = vectores.iter().filter(|x| x.header == headers[num_col])
+                    .flat_map(|x| x.col.iter()).zip(vectores.iter().filter(|x| x.header == col_i)
+                    .flat_map(|x| x.col.iter())).filter(|(x, y)| *x == valor_root && *y == valor_i)
+                    .count();
+                    apariciones_combinaciones.push(apariciones);                    
+                    //"   Apariciones de la combinación {nombre_columna} {valor_root} y {nombre_columna} {valor_i}: {apariciones}"
+                    println!("   Apariciones de la combinación {} {} y {} {}: {}", headers[num_col], valor_root, col_i, valor_i, apariciones);
+                }
+            }
+            
+            //CALCULAMOS LA PROBABILIDAD DE CADA COMBINACIÓN ROOT && I.
+            //APARIENCIA DE CADA COMBINACIÓN ROOT && I / APARICIONES DE CADA VALOR DE ROOT :
+            let mut probabilidad_combinaciones: Vec<f64> = Vec::new();
+            for (i, valor_root) in valores_unicos.iter().enumerate(){
+                for (_j, valor_i) in valores_unicos_i.iter().enumerate(){
+                    let apariciones = vectores.iter().filter(|x| x.header == headers[num_col])
+                    .flat_map(|x| x.col.iter()).zip(vectores.iter().filter(|x| x.header == col_i)
+                    .flat_map(|x| x.col.iter())).filter(|(x, y)| *x == valor_root && *y == valor_i)
+                    .count();
+                    probabilidad_combinaciones.push(apariciones as f64 / vec_apariciones[i].1 as f64);
+                    //"   Probabilidad de la combinación {nombre_columna} {valor_root} y {nombre_columna} {valor_i}: {probabilidad}"
+                    println!("   Probabilidad de la combinación {} {} y {} {}: {}", headers[num_col], valor_root, col_i, valor_i, apariciones as f64 / vec_apariciones[i].1 as f64);
+                }
+            }
+
+            //ENTROPIA DE CADA COMBINACIÓN ROOT && I:
+            let mut entropia_combinaciones: Vec<f64> = Vec::new();
+            for (i, prob) in probabilidad_combinaciones.iter().enumerate(){
+                //VALIDAMOS QUE LA ENTROPIA NO SEA 0
+                let entropia: f64;
+                if *prob == 0.0 {
+                    entropia = 0.0;
+                    entropia_combinaciones.push(entropia);
+                }
+                else{
+                    entropia = -prob * prob.log10();
+                    entropia_combinaciones.push(entropia);
+                }
+
+                //"   Entropía de la combinación {nombre_columna} {valor_root} y {nombre_columna} {valor_i}: {entropia}"
+                println!("   Entropía de la combinación {} {} y {} {}: {}", headers[num_col], combinaciones_valores_unicos[i].0, col_i, combinaciones_valores_unicos[i].1, entropia);
+            }
+            //ENTROPIA TOTAL DE LA COMBINACIÓN ROOT && I:
+            let entropia_total_combinaciones: f64 = entropia_combinaciones.iter().sum();
+            //"   Entropía total de la combinación {nombre_columna} {valor_root} y {nombre_columna} {valor_i}: {entropia_total}"
+            println!("   Entropía total de la combinación {} y {}: {}", headers[num_col], col_i, entropia_total_combinaciones);
+            
+
+
+
+
             let columnas_i = columnas.clone();
             //Eliminamos la columna actual
             let columnas_i: Vec<String> = columnas_i.into_iter().filter(|x| *x != col_i).collect();
 
             for col_j in columnas_i.clone() {
-                println!("        └─ Columna: {}", col_j); //To-delete
+                //"   └─── {padre}-{hija}-{nieta}", col_j
+                println!("      └─── {}-{}-{}", headers[num_col], col_i, col_j); //To-delete
+                
+
+                //CALCULOS DE COLUMNA J:
+                let valores_unicos_j = valores_unicos_por_columna[headers.iter().position(|x| *x == col_j).unwrap()].clone();
+                println!("        Valores únicos de la columna {}: {:?}", col_j, valores_unicos_j); //To-delete                
+                //COMBINACIONES DE LOS VALORES ÚNICOS DE LA COLUMNA ROOT, I y J Vec::<String, String, String> = (root[0], col_i[0], col_j[0]), (root[0], col_i[0], col_j[1]), (root[0], col_i[1], col_j[0]), (root[0], col_i[1], col_j[1]):
+                let mut combinaciones_valores_unicos: Vec<(String, String, String)> = Vec::new();   
+                for valor_root in valores_unicos.iter(){
+                    for valor_i in valores_unicos_i.iter(){
+                        for valor_j in valores_unicos_j.iter(){
+                            combinaciones_valores_unicos.push((valor_root.to_string(), valor_i.to_string(), valor_j.to_string()));
+                            //BUSCAMOS LAS APARICIONES DE CADA COMBINACIÓN ROOT && I && J: Usize (Sin reiterar, con lo que ya tenemos)
+                            let apariciones = vectores.iter().filter(|x| x.header == headers[num_col])
+                            .flat_map(|x| x.col.iter()).zip(vectores.iter().filter(|x| x.header == col_i)
+                            .flat_map(|x| x.col.iter())).zip(vectores.iter().filter(|x| x.header == col_j)
+                            .flat_map(|x| x.col.iter())).filter(|((x, y), z)| *x == valor_root && *y == valor_i && *z == valor_j)
+                            .count();
+                            //"      Apariciones de la combinación {nombre_columna} {valor_root} y {nombre_columna} {valor_i} y {nombre_columna} {valor_j}: {apariciones}"
+                            println!("        Apariciones de la combinación {} {} y {} {} y {} {}: {}", headers[num_col], valor_root, col_i, valor_i, col_j, valor_j, apariciones);
+                        }
+                    }
+                }              
+                
+                //CALCULAMOS LA PROBABILIDAD DE CADA COMBINACIÓN ROOT && I && J.
+                //APARIENCIA DE CADA COMBINACIÓN ROOT && I && J / APARICIONES DE CADA VALOR DE ROOT && I:
+                let mut indice_combinaciones: usize = 0;
+                let mut probabilidad_combinaciones: Vec<f64> = Vec::new();
+                for (_i, valor_root) in valores_unicos.iter().enumerate(){
+                    for (_j, valor_i) in valores_unicos_i.iter().enumerate(){
+                        for (_k, valor_j) in valores_unicos_j.iter().enumerate(){
+                            let apariciones = vectores.iter().filter(|x| x.header == headers[num_col])
+                            .flat_map(|x| x.col.iter()).zip(vectores.iter().filter(|x| x.header == col_i)
+                            .flat_map(|x| x.col.iter())).zip(vectores.iter().filter(|x| x.header == col_j)
+                            .flat_map(|x| x.col.iter())).filter(|((x, y), z)| *x == valor_root && *y == valor_i && *z == valor_j)
+                            .count();
+
+                            let div = apariciones_combinaciones[indice_combinaciones] as f64;                            
+                            let probabilidad = apariciones as f64 / div;
+
+                            probabilidad_combinaciones.push(probabilidad);                            
+
+                            //"      Probabilidad de la combinación {nombre_columna} {valor_root} y {nombre_columna} {valor_i} y {nombre_columna} {valor_j}: {probabilidad}"
+                            println!("        Probabilidad de la combinación {} {} y {} {} y {} {}: {}", headers[num_col], valor_root, col_i, valor_i, col_j, valor_j, probabilidad);
+                            // println!("          └─ {} / {}[{}]", apariciones, div, indice_combinaciones);
+                            // println!("          └─A: {:?}", apariciones_combinaciones); //To-delete
+                        }
+                        //EL índice de combinaciones se incrementa en 1 por cada ciclo completo de k
+                        indice_combinaciones += 1;
+                    }
+                }
+
+                //ENTROPIA DE CADA COMBINACIÓN ROOT && I && J:
+                let mut entropia_combinaciones: Vec<f64> = Vec::new();
+                for (i, prob) in probabilidad_combinaciones.iter().enumerate(){
+                    //VALIDAMOS QUE LA ENTROPIA NO SEA 0
+                    let entropia: f64;
+                    if *prob == 0.0 {
+                        entropia = 0.0;
+                        entropia_combinaciones.push(entropia);
+                    }
+                    else{
+                        entropia = -prob * prob.log10();
+                        entropia_combinaciones.push(entropia);
+                    }
+
+                    //"      Entropía de la combinación {nombre_columna} {valor_root} y {nombre_columna} {valor_i} y {nombre_columna} {valor_j}: {entropia}"
+                    println!("        Entropía de la combinación {} {} y {} {} y {} {}: {}", headers[num_col], combinaciones_valores_unicos[i].0, col_i, combinaciones_valores_unicos[i].1, col_j, combinaciones_valores_unicos[i].2, entropia);
+                }
+                
+
+
+
 
                 let columnas_j = columnas_i.clone();
                 //Eliminamos la columna actual
@@ -64,56 +254,39 @@ fn main() {
     }
 
     //VERSIÓN DINÁMICA:
-    println!("\n########################----VERSIÓN DINÁMICA----########################");
-    let columnas: Vec<String> = headers.clone();
-    let mut contador_arboles = 0;
-    let n_headers = headers.len();
-    // arbol(headers.len(), headers, vectores, combinaciones, 0);
-    arbol_recursivo(n_headers, headers, &mut contador_arboles, 0, n_headers);
+    // println!("\n########################----VERSIÓN DINÁMICA----########################");
+    // let columnas: Vec<String> = headers.clone();
+    // let mut contador_arboles = 0;
+    // let n_headers = headers.len();
+    // // arbol(headers.len(), headers, vectores, combinaciones, 0);
+    // arbol_recursivo(n_headers, headers, &mut contador_arboles, 0, n_headers);
     
 
 
 }
 
-fn arbol_recursivo(n_headers: usize, headers: Vec<String>, contador_arboles: &mut usize, depth: usize, n_headers_original: usize) {
-    if n_headers != 0 {
-        for i_header in 0..headers.len() {
-            if n_headers == n_headers_original{
-                println!("───{}Columna: {}" , "    ".repeat(depth), headers[i_header]);
-            }
-            else if n_headers == 1 {
-                *contador_arboles += 1;
-                println!("{}└─Columna: {}.({})", "     ".repeat(depth), headers[i_header], *contador_arboles);
-            }
-            
-            else{
-                println!("{}└─Columna: {}", "     ".repeat(depth), headers[i_header]);
-            }
-
-
-            let mut new_headers = headers.clone();
-            //Eliminamos la columna actual que coincida con el valor de headers[i_header]:
-            new_headers = new_headers.into_iter().filter(|x| *x != headers[i_header]).collect();
-            
-            arbol_recursivo(n_headers - 1, new_headers, contador_arboles, depth + 1, n_headers_original);
-        }
-    }
-}
-
-
-
-// fn arbol(n_headers: usize, headers: Vec<String>, vectores: Vec<Vector>, n_arboles: usize, contador_arboles: usize){
-//     if n_arboles != contador_arboles {
+// fn arbol_recursivo(n_headers: usize, headers: Vec<String>, contador_arboles: &mut usize, depth: usize, n_headers_original: usize) {
+//     if n_headers != 0 {
 //         for i_header in 0..headers.len() {
-//             println!("{}:{}Columna: {}", contador_arboles, "    ".repeat(i_header), headers[i_header]);
+//             if n_headers == n_headers_original{
+//                 println!("───{}Columna: {}" , "    ".repeat(depth), headers[i_header]);
+//             }
+//             else if n_headers == 1 {
+//                 *contador_arboles += 1;
+//                 println!("{}└─Columna: {}.({})", "     ".repeat(depth), headers[i_header], *contador_arboles);
+//             }
+            
+//             else{
+//                 println!("{}└─Columna: {}", "     ".repeat(depth), headers[i_header]);
+//             }
+
+
 //             let mut new_headers = headers.clone();
-//             //Eliminamos la columna actual que coincida con el valor de headers[num_col]:
+//             //Eliminamos la columna actual que coincida con el valor de headers[i_header]:
 //             new_headers = new_headers.into_iter().filter(|x| *x != headers[i_header]).collect();
-//             arbol(n_headers, new_headers, vectores.clone(), n_arboles, contador_arboles+i_header)
+            
+//             arbol_recursivo(n_headers - 1, new_headers, contador_arboles, depth + 1, n_headers_original);
 //         }
-//     }
-//     else {
-//         return;
 //     }
 // }
 
